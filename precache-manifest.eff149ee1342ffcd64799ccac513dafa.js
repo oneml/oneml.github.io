49849bb97ecb7e07b0a8a678540d18c82ec15957
oneml <72938177+oneml@users.noreply.github.com>
2021-01-12 15:39:01 +0530

    Add files via upload

---
+++ precache-manifest.eff149ee1342ffcd64799ccac513dafa.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = [
+  {
+    "revision": "9db9c695789eff50f2e4",
+    "url": "/static/css/main.8357f81d.chunk.css"
+  },
+  {
+    "revision": "9db9c695789eff50f2e4",
+    "url": "/static/js/main.a0767009.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1d5667754ff1aac8cddf",
+    "url": "/static/css/2.e466b824.chunk.css"
+  },
+  {
+    "revision": "1d5667754ff1aac8cddf",
+    "url": "/static/js/2.0b3ec4d7.chunk.js"
+  },
+  {
+    "revision": "819a1cfbdf4931cd19c4106b4419e9fb",
+    "url": "/static/media/GradientDescent.819a1cfb.ipynb"
+  },
+  {
+    "revision": "15952de05521ef159698ad1a25ea55a8",
+    "url": "/static/media/Linear_Regression.15952de0.ipynb"
+  },
+  {
+    "revision": "2810a88b1d6365b363caeabe678f1629",
+    "url": "/static/media/logo.2810a88b.svg"
+  },
+  {
+    "revision": "580f4beb1b393742023fd786f2f475e6",
+    "url": "/static/media/logo.580f4beb.jpg"
+  },
+  {
+    "revision": "a08cfac7bbdd3fd37c8a2601573d2bc7",
+    "url": "/static/media/regression.a08cfac7.png"
+  },
+  {
+    "revision": "a8be513f571a0ba1ab6ba139a60f6022",
+    "url": "/static/media/segmentation.a8be513f.png"
+  },
+  {
+    "revision": "70c9719f93749d26b39f5b848abbbb10",
+    "url": "/static/media/object.70c9719f.png"
+  },
+  {
+    "revision": "ac06c29344f8ae4b1284170f423582d0",
+    "url": "/static/media/image.ac06c293.gif"
+  },
+  {
+    "revision": "c61471d05c28ecd551f1739f6a14ea0d",
+    "url": "/static/media/categorical.c61471d0.png"
+  },
+  {
+    "revision": "ac9d19c3b1f441316f3039cc3cff4bbf",
+    "url": "/static/media/thug.ac9d19c3.jpg"
+  },
+  {
+    "revision": "e0bc53dffe4e6ceee9fa7f44d76bb243",
+    "url": "/static/media/LR1.e0bc53df.jpg"
+  },
+  {
+    "revision": "ea54fc09216f246ac62789facb2aa13d",
+    "url": "/static/media/LR2.ea54fc09.jpg"
+  },
+  {
+    "revision": "8fb6107888cc85aad7609a0303d24851",
+    "url": "/static/media/LR3.8fb61078.jpg"
+  },
+  {
+    "revision": "d92b589826a3d9f4d3bf8deba2277237",
+    "url": "/static/media/CC1.d92b5898.jpeg"
+  },
+  {
+    "revision": "1e7ecb9464e8580887ca8955cea5de67",
+    "url": "/static/media/CC2.1e7ecb94.jpeg"
+  },
+  {
+    "revision": "5d8781093f532a91437a15a2d44189de",
+    "url": "/static/media/CC3.5d878109.jpeg"
+  },
+  {
+    "revision": "bd319f9fdf982fcbf1eca52191a66320",
+    "url": "/static/media/CC4.bd319f9f.jpeg"
+  },
+  {
+    "revision": "1d450e771f30efa92ee2dafe93f0472c",
+    "url": "/static/media/CC5.1d450e77.jpeg"
+  },
+  {
+    "revision": "891361a7ffa2c5a3739bc75d868db608",
+    "url": "/static/media/IS1.891361a7.jpg"
+  },
+  {
+    "revision": "098293281fbce34b4d4c17e5d71cee8d",
+    "url": "/static/media/IS2.09829328.jpg"
+  },
+  {
+    "revision": "4ad8eacbc0824804c15a66b0548fd6f5",
+    "url": "/static/media/IS3.4ad8eacb.jpg"
+  },
+  {
+    "revision": "32e52ad927e7cfc98728f792d87b9e83",
+    "url": "/static/media/Od1.32e52ad9.jpeg"
+  },
+  {
+    "revision": "855a75d9d43bf646b242d7e9d31039f0",
+    "url": "/static/media/ODf.855a75d9.jpeg"
+  },
+  {
+    "revision": "c70f46c6c5cb5f4652873d0e4c234d91",
+    "url": "/static/media/IC2.c70f46c6.jpg"
+  },
+  {
+    "revision": "7565e134c24c8574588c1a2c8225f1e3",
+    "url": "/static/media/IC3.7565e134.jpg"
+  },
+  {
+    "revision": "e86fee578fa5ba26ef21377bab9cf67c",
+    "url": "/static/media/TL3.e86fee57.jpg"
+  },
+  {
+    "revision": "6a2840d0f806932276c3ddf85344c397",
+    "url": "/static/media/TL1.6a2840d0.jpg"
+  },
+  {
+    "revision": "13ee51527a9c467648c7310dcff6e247",
+    "url": "/static/media/TL2.13ee5152.jpg"
+  },
+  {
+    "revision": "6816f60b7f8888fe735ca5fc2b2870f3",
+    "url": "/static/media/Logistic Regression.6816f60b.ipynb"
+  },
+  {
+    "revision": "aea3dfffc555989a87a385e80023acec",
+    "url": "/static/media/output-word.aea3dfff.jpg"
+  },
+  {
+    "revision": "acdf677e0d8a928a291e73f71d80b746",
+    "url": "/static/media/Image Segmentation.acdf677e.ipynb"
+  },
+  {
+    "revision": "3dc8990d0be02c86460187b8e4b2d94b",
+    "url": "/index.html"
+  }
+];
